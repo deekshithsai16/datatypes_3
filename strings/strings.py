b8de6a8f7fe61649f2fb660c98dd6029063a4c8d
deekshithsai16 <deekshithsai08@cloud.com>
2025-11-27 19:40:32 +0530

    practice

---
+++ strings/strings.py
@@ -0,0 +1,11 @@
+s_ch='v'
+print(s_ch)
+print(type(s_ch))
+
+t_ch="vj"
+print(t_ch)
+print(type(t_ch))
+
+m_line=''' I am deekshith studying in MITS Deemed to be University in Madanapalle'''
+print(m_line)
+print(type(m_line))
